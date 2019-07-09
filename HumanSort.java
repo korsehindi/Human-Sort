30c2cc9595bf4f76c2578ed06fdd18ce507fecda
BlackSamurai <48545796+korsehindi@users.noreply.github.com>
2019-07-09 10:47:59 +0430

    Create HumanSort.java

---
+++ HumanSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SimpleHumanLikeSort {
+
+    public static void main(String arguments[]){
+
+        int ages[] = {21, 42, 56, 5, 4};
+
+        boolean swapped;
+        do{
+            swapped = false;
+            for(int i = 0; i < ages.length-1; i++){
+                int i1 = i + 1;
+
+                if (ages[i] > ages[i1]){
+                    // swap
+                    int tmp = ages[i];
+                    ages[i] = ages[i1];
+                    ages[i1] = tmp;
+                    swapped = true;
+                }
+            }
+            System.out.println(Arrays.toString(ages));
+        } while(swapped);
+
+    }
+}
