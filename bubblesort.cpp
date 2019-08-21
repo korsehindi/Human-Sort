73cd30891bdecc3cd6e3c0716d9092482ae28959
Black Samurai <48545796+korsehindi@users.noreply.github.com>
2019-08-21 12:57:03 +0430

    Create bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,15 @@
+// ------- > https://www.tutorialspoint.com/cplusplus-program-to-implement-bubble-sort
+
+void bubbleSort(int *array, int size) {
+   for(int i = 0; i<size; i++) {
+      int swaps = 0;         //flag to detect any swap is there or not
+      for(int j = 0; j<size-i-1; j++) {
+         if(array[j] > array[j+1]) {       //when the current item is bigger than next
+            swapping(array[j], array[j+1]);
+            swaps = 1;    //set swap flag
+         }
+      }
+      if(!swaps)
+         break;       // No swap in this pass, so array is sorted
+   }
+}
